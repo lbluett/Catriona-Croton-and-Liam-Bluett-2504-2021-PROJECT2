1ef42212cc0319a162501531eca9540a8e0f5534
CCroton <88709406+CCroton@users.noreply.github.com>
2021-10-08 07:29:50 +1000

    Update Proj2_misc_functions_v1_CC.jl
    Added stitch function from Unit 6 notes for discrete event plots

--- src/Proj2_misc_functions_v1_CC.jl
+++ src/Proj2_misc_functions_v1_CC.jl
@@ -1,9 +1,24 @@
  # Miscellaneous functions
 
 """
-A convenience function to make a Gamma distribution with desired rate (inverse of shape) and SCV.
+ Function to make a Gamma distribution with desired rate (inverse of shape) and SCV
 """
 rate_scv_gamma(desired_rate::Float64, desired_scv::Float64) = Gamma(1/desired_scv, desired_scv/desired_rate)
  # for this simulation, the SCV will be 3
 
- 
+
+"""
+Function to stich_steps of a discrete event curve
+"""
+function stich_steps(epochs, values)
+    n = length(epochs)
+    new_epochs  = [epochs[1]]
+    new_values = [values[1]]
+    for i in 2:n
+        push!(new_epochs, epochs[i])
+        push!(new_values, values[i-1])
+        push!(new_epochs, epochs[i])
+        push!(new_values, values[i])
+    end
+    return (new_epochs, new_values)
+end
