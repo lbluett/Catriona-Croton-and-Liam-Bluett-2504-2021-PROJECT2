d33a1c9c9f74727139b69dcbc7fa660fac548423
CCroton <88709406+CCroton@users.noreply.github.com>
2021-10-08 08:02:45 +1000

    Create scenario_2.jl

---
+++ test/scenario_2.jl
@@ -0,0 +1,12 @@
+scenario2 = NetworkParameters(  L=3, 
+                                gamma_shape = 3.0, 
+                                λ = NaN, 
+                                η = 4.0, 
+                                μ_vector = ones(3),
+                                P = [0 1.0 0;
+                                    0 0 1.0;
+                                    0.5 0 0],
+                                Q = zeros(3,3),
+                                p_e = [1.0, 0, 0],
+                                K = fill(5,3))
+@show scenario2
