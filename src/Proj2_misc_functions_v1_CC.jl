5273edbb7131b78c166cd9e1870e9640420bc7f5
CCroton <88709406+CCroton@users.noreply.github.com>
2021-10-10 12:16:36 +1000

    Update Proj2_misc_functions_v1_CC.jl
    added proposed inverse probability transform for using P and Q matrices

--- src/Proj2_misc_functions_v1_CC.jl
+++ src/Proj2_misc_functions_v1_CC.jl
@@ -22,3 +22,23 @@ function stich_steps(epochs, values)
     end
     return (new_epochs, new_values)
 end
+
+"""
+Function to use inverse probability transform for P and Q matrices
+    Generates discrete random value given probability vector
+"""
+# prob vector is the vector of probabilities
+# for the park, will be the i'th line from the P or Q matrices
+function discrete_inverse_transform_sampling(prob_vector::Array{Float64, 1})
+    # randomly generate a probability value between 0 and 1
+    prob = rand()
+    # then use inverse probability transform to find the category index the probability belongs to
+    if prob <= prob_vector[1, ]
+        return 1
+    end
+    else for i in 2:length(prob_vector)
+        if(sum(prob_vector[1: (i-1)]) < prob && prob <= sum(prob_vector[1:i]))
+            return i
+        end
+    end
+end
