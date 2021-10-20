f73cce54abcab32343bfa74a76e1a4814aa58962
CCroton <88709406+CCroton@users.noreply.github.com>
2021-10-20 16:43:54 +1000

    Delete Proj2_functions_v1_CC.jl

--- src/Proj2_functions_v1_CC.jl
+++
@@ -1,4 +0,0 @@
-# General functions
-
-next_park_arrival_time(s::State) = rand(rate_scv_gamma(s.params.λ, s.params.gamma_shape))
-next_service_time(s::State, q::Int) = rand(rate_scv_gamma(s.params.μ_array[q], s.params.gamma_shape));
