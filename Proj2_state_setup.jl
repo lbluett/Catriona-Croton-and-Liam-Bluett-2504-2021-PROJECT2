63f69122b75be9678005f7fb9639717c5a15e7a1
CCroton <88709406+CCroton@users.noreply.github.com>
2021-10-20 16:41:04 +1000

    Add files via upload
    Merging of Liam and first branch material, up to trajectory code and testing

---
+++ Proj2_state_setup.jl
@@ -0,0 +1,39 @@
+using Base: Float64
+# To define the possible states in the Park system
+
+# Declare the abstract type called state
+abstract type State end
+
+# before arrival at park
+mutable struct BeforeArrival <: State
+end
+
+# arrive at park
+mutable struct ArrivePark <: State
+    number_arrive_park::Int
+end
+
+# queue at station state
+mutable struct QueueState <: State
+    number_in_queue::Int
+end
+
+# being served at station state
+mutable struct ServeState <: State
+    number_serve::Int
+end
+
+# moving between stations after served state
+mutable struct ServeMoveState <: State
+    number_moving_after_serve::Int
+end
+
+# moving between stations after overflowed state
+mutable struct OverflowMoveState <: State
+    number_moving_after_overflow::Int
+end
+
+# leave park state
+mutable struct LeaveParkState <: State
+    number_leave_park::Int
+end
